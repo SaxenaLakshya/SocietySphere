4fc626b2be9a1e51e167fb7f67883ef02b0db455
LakshyaSaxena <saxena.lakshya0106@gmail.com>
2026-02-08 15:21:03 +0530

    cleared some unwanted stuff after clerk integration

--- components/index.ts
+++ components/index.ts
@@ -2,14 +2,10 @@ import FeatureCard from "./FeatureCard";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import GlobalAlert from "./GlobalAlert";
-import UserRegisterForm from "./UserRegisterForm";
-import AdminRegisterForm from "./AdminRegisterForm";
 
 export {
     FeatureCard,
     Navbar,
     Footer,
     GlobalAlert,
-    UserRegisterForm,
-    AdminRegisterForm,
 };
--- types/index.ts
+++ types/index.ts
@@ -5,35 +5,6 @@ export interface FeatureCardProps {
     description: string,
 };
 
-export type RegisterFormInputs = {
-    fullName: string,
-    email: string,
-    mobileNumber: number,
-    societyCode: string,
-    role: "resident" | "tenant" | "staff",
-    password: string,
-    terms: boolean,
-};
-
-export type AdminFormInputs = {
-    fullName: string,
-    email: string,
-    mobileNumber: number,
-    societyName: string,
-    city: string,
-    state: string,
-    towers: number,
-    houses: number,
-    password: string,
-    terms: boolean,
-};
-
-export type LoginFormInput = {
-    email: string,
-    password: string,
-    rememberMe: boolean,
-};
-
 export type AlertType = "info" | "success" | "warning" | "error"
 
 export type GlobalAlertProps = {
